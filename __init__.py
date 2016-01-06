1195b5fbf6b6e9132be3dd177fd3641bea66b882
troyhirni <troyhirni@yahoo.com>
2016-01-06 01:30:04 -0600

    Add fmt.Grid; Reorganize fmt to accomodate.

--- __init__.py
+++ __init__.py
@@ -4,20 +4,27 @@ This file is part of the pyrox project, distributed under
 the terms of the GNU Affero General Public License.
 """
 
-from .base import fs
+from .base import fs, url, text
 from .base import prompt as prompt_
 
-# fs
-def path(*a, **k):
-	return fs.Path(*a, **k)
-
-def file(*a, **k):
-	return fs.File(*a, **k)
+#
+# TEST FUNCTIONS
+#
 
 def dir(*a, **k):
 	return fs.Dir(*a, **k)
 
+def file(*a, **k):
+	return fs.File(*a, **k)
+
+def text(*a, **k):
+	return text.text(*a, **k)
+
+def url(*a, **k):
+	return url.open(*a, **k)
+
+
+# PROMPT
 def prompt(*a, **k):
 	p = prompt_.Prompt(*a, **k)
 	return p.prompt()
-
--- base/fmt.py
+++ base/fmt.py
@@ -60,10 +60,17 @@ class Format(FormatBase):
 		return self.args[0].format(*a, **k)
 
 
+
 #
-# JSON - FOR DISPLAY
+# JSON FORMAT
 #
-class JFormat(FormatBase):
+class JSON(FormatBase):
+	def format(self, data):
+		"""Format data as json strings in a pretty format."""
+		return json.dumps(data, **self.kwargs)
+
+	
+class JFormat(JSON):
 	"""JSON in display format."""
 	
 	def __init__(self, **k):
@@ -77,26 +84,20 @@ class JFormat(FormatBase):
 		return json.dumps(data, **self.kwargs)
 
 
-#
-# JSON - COMPACT
-#
-class JCompact(JFormat):
+class JCompact(JSON):
 	"""Compact JSON format."""
 	
 	def __init__(self, **k):
 		"""Kwargs are passed directly to json.dump(s) functions."""
-		k.setdefault('indent', 0)
 		k.setdefault('separators', (',',':'))
-		JFormat.__init__(self, **k)
+		JSON.__init__(self, **k)
 
 	def format(self, data):
-		"""Format as compact json; no unnecessary white."""
+		#Format as compact json; no unnecessary white.
 		return ''.join(json.dumps(data, **self.kwargs).splitlines())
 
 
-#
-# JSON ENCODER
-#
+# JSON - Utility
 class JSONDisplay(json.JSONEncoder):
 	"""
 	Handles unparsable types by returning their representation to be
@@ -109,3 +110,55 @@ class JSONDisplay(json.JSONEncoder):
 			return repr(obj)
 
 
+
+#
+# SPECIALTY
+#
+class Grid (FormatBase):
+	
+	def __init__(self, sep=' ', **k):
+		self.__sep = sep
+		self.__fmt = k.get('cellformat') or str
+		FormatBase.__init__(self)
+	
+	"""Format list of lists (grid) into a grid."""
+	def formatstring(self, grid):
+		glen = len(grid[0])
+		cmax = [0 for x in range(glen)]
+		for row in grid:
+			for c,col in enumerate(row):
+				L = len(col)
+				if L > cmax[c]:
+					cmax[c] = L
+		
+		fstr = map(lambda x: "{:<%s}" % x, [cmax[x] for x in range(0, glen)])
+		return self.__sep.join(fstr)
+	
+	def formatgrid(self, grid, fmt):
+		"""Duplicate grid, but with each item formatted by fmt."""
+		
+		# duplicate grid with formatted items
+		fgrid = []
+		for gRow in grid:
+			cols = []
+			for col in gRow:
+				# copy each cell from grid to fgrid, but formatted
+				cols.append(fmt(col))
+			fgrid.append(cols)
+		
+		# return the copy
+		return fgrid
+	
+	
+	def format(self, grid, **k):	
+		
+		# format grid
+		fmt = k.get("cellformat", self.__fmt)
+		if fmt:
+			grid = self.formatgrid(grid, fmt)
+		
+		r = []
+		f = self.formatstring(grid)
+		for row in grid:
+			r.append(f.format(*row))
+		return '\n'.join(r)
