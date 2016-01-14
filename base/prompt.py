64f5970e12c6f10626d9c421861a3f7d76b29ff2
troyhirni <troyhirni@yahoo.com>
2016-01-13 23:53:04 -0600

    Restructure base.prompt in preparation for future changes

--- base/prompt.py
+++ base/prompt.py
@@ -3,16 +3,15 @@ Copyright 2014-2015 Troy Hirni
 This file is part of the pyrox project, distributed under
 the terms of the GNU Affero General Public License.
 
-Prompt - Interact with python objects as though in a shell.
+Prompt - Commind-line Interface
 """
 
 try:
-	from ..base import *
+	from pyrox.base import *
 except:
 	from base import *
 
-from . import fmt
-
+from pyrox.base import scan
 
 import time
 
@@ -21,81 +20,171 @@ DEF_PROMPT = '> '
 DEF_GUIDE = "Ctrl-c to exit."
 
 
-#
-# PROMPT
-#
+
+
+def prompt(obj, guide=DEF_GUIDE, **k):
+	"""Access object 'obj' via internal command line prompt."""
+	return Prompt(obj, **k).prompt(guide)
+
+
+
+
+
 class Prompt(object):
-	
-	def __init__(self, o=None, **k):
-		self.__o = o if o else self
-		self.__p = k.get('prompt', DEF_PROMPT)
-		self.__m = k.get('guide', DEF_GUIDE)
-		self.__f = k.get('format', None)
+	def __init__(self, o, **k):
+		self.__target = o
+		self.__input = k.get('input', PromptInput())
+		self.__parse = k.get('parse', PromptParse())
+		self.__handle = k.get('handle', PromptHandle(self, o))
+		self.__output = k.get('output', PromptOutput())
+		
+		T = type(o)
+		self.__input.ident = "%s.%s" % (T.__module__, T.__name__)
 	
 	@property
-	def dir(self):
-		return dir(self.__o)
+	def target(self):
+		return self.__target
 	
 	@property
-	def dict(self):
-		return self.__o.__dict__
+	def input(self):
+		return self.__input
 	
-	# INPUT
-	def input(self, p=None):
+	@property
+	def output(self):
+		return self.__output
+	
+	@property
+	def parse(self):
+		return self.__parse
+	
+	@property
+	def handle(self):
+		return self.__handle
+	
+	def prompt(self, guide=DEF_GUIDE):
+		"""Read and handle input from terminal or python interpreter."""
+		result = None
+		if guide:
+			self.output(guide)
 		try:
-			return textinput(p if p else self.__p)
+			# loop until ctrl-c
+			while True:
+				line = self.input()
+				
+				# this needs to come after line so that the final result
+				# will be returned if ctrl-c is pressed
+				result = None
+				
+				# parse and handle command
+				if line.strip():
+					command = self.parse(line)
+					try:
+						result = self.handle(command)
+					except Exception as ex:
+						#print (ex)
+						raise
+						
+					# show the result of each command
+					if result != None:
+						self.output (result) 
+				
+		except KeyboardInterrupt:
+			# print to get to the next line
+			print()
+			
+		# return the result of the last command
+		return result
+
+
+
+
+
+class PromptHandle(object):
+	def __init__(self, prompt, target, **k):
+		self.target = target
+		self.prompt = prompt
+		
+	def __call__(self, cmd):
+		return self.handle(cmd)
+	
+	def handle(self, cmd, *a, **k):
+		prop = getattr(self.target, cmd['cmd'])
+		args = cmd['args']
+		try:
+			r = prop(*args)
+		except TypeError:
+			r = prop
+		return r
+
+
+
+
+
+class PromptInput(object):
+	def __init__(self, prompt=DEF_PROMPT, **k):
+		self.__prompt = k.get('prompt', prompt)
+	
+	def __call__(self):
+		try:
+			return textinput(self.prompt)
 		except EOFError:
 			time.sleep(1) # relief for win8+/py2 interpreter bug
 	
-	# OUTPUT
-	def output(self, x):
-		print(self.__f(x) if self.__f else x)
+	@property
+	def prompt(self):
+		return self.__prompt
 	
-	# PARSE (command line)
-	def parse(self, s):
-		return s.lstrip().split()
+	@prompt.setter
+	def prompt(self, prompt):
+		self.__prompt = prompt
+		self.__resetprompt()
 	
-	# PROMPT
-	def prompt(self, **k):
-		o = self.__o if self.__o else self
-		p = k.get('prompt', self.__p)
-		m = k.get('guide', self.__m)
-		r = None
-		if m:
-			print (m)
-		try:
-			while True:
-				s = self.input(p)
-				r = None
-				if s.strip():
-					line = self.parse(s)
-					args = line[1:]
-					cmd = line[0]
-					try:
-						m = getattr(o, cmd)
-						r = m(*args)
-					except TypeError:
-						r = m
-					except Exception as ex:
-						print (str(ex))
-				if r != None:
-					self.output (r) 
-		except KeyboardInterrupt:
-			print()
-			return r
+	@property
+	def ident(self):
+		return self.__ident
 	
-	# TEXT
-	def text(self, **k):
+	@ident.setter
+	def ident(self, id):
+		self.__ident = id if id else ''
+		self.__resetprompt()
+		
+	def __resetprompt(self):
+		self.__prompt = "%s%s" % (self.__ident, self.__prompt)
+
+
+
+
+
+class PromptText(object):
+	
+	def __call__(self):
 		a = []
-		p = k.get('prompt', DEF_PROMPT)
-		m = k.get('guide', DEF_GUIDE)
-		if m:
-			print (m)
 		try:
 			while True:
-				a.append(input(p))
+				a.append(textinput(self.prompt))
 		except KeyboardInterrupt:
 			pass
 		return a
+
+
+
+
+
+class PromptOutput(object):
+	def __call__(self, x):
+		print (x)
+
+
+
+
+
+class PromptParse(object):
+	"""Incredibly simple line parser."""
+	def __call__(self, line):
+		r = {}
+		cl = line.lstrip().split()
+		r['cmd'] = cl[0]
+		r['args'] = cl[1:]
+		return r
 
 
