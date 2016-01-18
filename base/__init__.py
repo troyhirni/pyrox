13025e567b0cded0c0c02e417d5c5bfafcad9b09
troyhirni <troyhirni@yahoo.com>
2016-01-18 16:07:52 -0600

    Fix base.udate bugs for python 3

--- base/__init__.py
+++ base/__init__.py
@@ -4,8 +4,8 @@ This file is part of the pyrox project, distributed under
 the terms of the GNU Affero General Public License.
 
 BASE - Defintions needed by many modules in this package. Expect 
-			 many changes here during early experimental development. In
-			 the end, it will consist of most basic needs.
+       many changes here during early experimental development. In
+       the end, it will consist of most basic needs.
 """
 
 import struct
@@ -18,11 +18,8 @@ try:
 except:
 	from imp import reload
 	basestring = unicode = str
+	unichr = chr
 	textinput = input
-	
-	def unichr(i):
-		return struct.pack('i', i).decode('utf-32') 
-
 
 
 DEF_ENCODE = 'utf-8'
