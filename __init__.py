7add5fe4c90234ad644a771e06169e61f8f24d35
Troy Hirni <troyhirni@yahoo.com>
2016-12-29 09:35:24 -0600

    remove commented test functions

--- __init__.py
+++ __init__.py
@@ -1,32 +1,5 @@
 """
-Copyright 2015 Troy Hirni
+Copyright 2015-2016 Troy Hirni
 This file is part of the pyrox project, distributed under
 the terms of the GNU Affero General Public License.
-"""
-
-"""
-from .base import fs, url, text
-from .base import prompt as prompt_
-
-#
-# TEST FUNCTIONS
-#
-
-def dir(*a, **k):
-	return fs.Dir(*a, **k)
-
-def file(*a, **k):
-	return fs.File(*a, **k)
-
-def text(*a, **k):
-	return text.text(*a, **k)
-
-def url(*a, **k):
-	return url.open(*a, **k)
-
-
-# PROMPT
-def prompt(*a, **k):
-	p = prompt_.Prompt(*a, **k)
-	return p.prompt()
 """
