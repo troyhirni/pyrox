59d37332c5364a22f16da1e777cc0a6a19f31720
Troy Hirni <troyhirni@yahoo.com>
2017-01-13 22:32:45 -0600

    Update comments, imports

--- fs/config.py
+++ fs/config.py
@@ -1,3 +1,11 @@
+"""
+Copyright 2014-2017 Troy Hirni
+This file is part of the pyrox project, distributed under
+the terms of the GNU Affero General Public License.
+
+CONFIG - Read/Write config files
+
+"""
 
 
 import ast, json
