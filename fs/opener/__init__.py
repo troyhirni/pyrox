8ab90152244f285b091cbf493a39766b560f1080
Troy Hirni <troyhirni@yahoo.com>
2017-01-13 22:41:15 -0600

    Update comments

--- fs/opener/__init__.py
+++ fs/opener/__init__.py
@@ -1,3 +1,15 @@
+"""
+Copyright 2017 Troy Hirni
+This file is part of the pyrox project, distributed under
+the terms of the GNU Affero General Public License.
+
+OPENER - Opening files
+
+Chooses the most recently available method for opening files. For 
+python 2.7 and later, this means the io module's open function. For
+earlier versions, the codecs module or python's built-in open() 
+function or file() constructor may be used.
+"""
 
 try:
 	from ._io import Opener
--- fs/opener/_codecs.py
+++ fs/opener/_codecs.py
@@ -1,3 +1,8 @@
+"""
+Copyright 2017 Troy Hirni
+This file is part of the pyrox project, distributed under
+the terms of the GNU Affero General Public License.
+"""
 
 import codecs
 
